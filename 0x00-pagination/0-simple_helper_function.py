128e3495ab9e605734003250057dcb9ddf7f3dbb
phurhard <phurhardeen@gmail.com>
2023-08-25 06:43:28 +0100

    step1

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" A simple helper function to facilitate pagination"""
+
+
+def index_range(page, page_size):
+    """ This function returns a tuple of the ramge of indexes given a page number amd page size"""
+    start_index = (page_size * page) - page_size
+    end_index = page_size * page
+    return (start_index, end_index)
---
+++ 0x00-pagination/README.md
@@ -0,0 +1,3 @@
+# Pagination is tye process of sectionig a large portion of datasets into smaller chunks to ease access on both the clients and servers side
+- There are quite a few types of pagination depending on use case
+* [x] seek pagination 
