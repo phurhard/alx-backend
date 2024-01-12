dbd486aec4a09eea10e2faebf34553f248853747
phurhard <phurhardeen@gmail.com>
2024-01-12 06:16:57 +0100

    try

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,17 +7,22 @@ from flask_babel import Babel
 class Config:
     """Configuration class"""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# def get_locale():
+#     """Set the default locale and timestamp"""
+#     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+# def get_timezone():
+#     return user.timezone
 
 
 app = Flask(__name__)
 babel = Babel(app)
 app.config.from_object(Config)
-
-
-@babel.localeselector
-def get_locale():
-    """Set the default locale and timestamp"""
-    return "en"
 
 
 @app.route('/')
