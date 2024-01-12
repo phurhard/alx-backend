94da81fa7cb310e2a651a7b5f8e5ec789c349027
phurhard <phurhardeen@gmail.com>
2024-01-12 10:36:29 +0100

    saple

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 """Flask Babel internalization and localization module"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config:
     """Configuration class"""
     LANGUAGES = ["en", "fr"]
-    # BABEL_DEFAULT_LOCALE = "en"
-    # BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
@@ -31,11 +29,9 @@ def get_locale():
 @app.route('/')
 def hello():
     """This is a basic setup of flask"""
-    # title = _('Welcome to Holberton')
-    # header = _('Hello world!')
     return render_template("4-index.html")
 
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=1)
