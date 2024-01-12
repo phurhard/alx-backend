6bc6d1333be8f98c59398022815761864894b4cf
phurhard <phurhardeen@gmail.com>
2024-01-12 10:20:19 +0100

    docs

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-"""Flask Babel internalization and
-localization module"""
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
@@ -20,13 +18,13 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """Set the default locale and timestamp"""
+    """Set the default locale and timestamp based on user preferences"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
 def hello():
-    """This is a basic setup of flask"""
+    """Render the home page with internationalized content"""
     title = _('Welcome to Holberton')
     header = _('Hello world!')
     return render_template("3-index.html", title=title, header=header)
