8a594f619812ebb2c50319acf005f04021f8ac78
phurhard <phurhardeen@gmail.com>
2024-02-08 06:13:44 +0100

    do not understand this again

--- 0x03-queuing_system_in_js/0-redis-client.js
+++ 0x03-queuing_system_in_js/0-redis-client.js
@@ -1,5 +1,8 @@
 import { createClient} from 'redis'
 
 const client = createClient()
-client.isReady("success", res => {console.log("Redis client connected to the server", res)})
-client.on("error", err => {console.log("Redis client not connected to the server: ", err)})
+if (client.connected) {
+    console.log("Redis client connected to the server")
+} else {
+client.on("connection", (err) => {console.log("Redis client not connected to the server: ", err)})
+}
