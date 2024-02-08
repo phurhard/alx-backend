84862bb9377965462c89846f242e999b717d7e69
phurhard <phurhardeen@gmail.com>
2024-02-08 13:08:35 +0100

    1-redis-op

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,31 @@
+import { redisClientFactory } from 'kue';
+import { createClient } from 'redis';
+import redis from 'redis';
+
+const client = createClient();
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+  });
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server', err);
+});
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+    client.get(schoolName, (err, result) => {
+        if (err) {
+            console.log('Error', err);
+        } else {
+            console.log(result);
+        }
+    });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+client.quit();
